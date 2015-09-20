233f70ce4cf3095b41e7a441edcf67e84296acbe
ryaan <rtulino@gmail.com>
2015-09-20 12:09:11 -0400

    Update HelloTest.php

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -4,6 +4,9 @@ class HelloTest extends PHPUnit_Framework_TestCase
     public function testHello()
     {
         $hello = new Test/Hello();
+        
+        $hello->setMessage('Hello World');
+        
         $this->assertEquals('Hello World', $hello->getMessage());
     }
 }
